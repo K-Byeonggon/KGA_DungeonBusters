ea5fb57330c6d03a22d18f23bd19a3adc00810f4
K_Byeonggon <govleen79@gmail.com>
2024-06-17 17:54:24 +0900

    [feat] 로그인 기능 완성

---
+++ Assets/Scripts/LoginUI.cs
@@ -0,0 +1,59 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginUI : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] InputField Input_Id;
+    [SerializeField] InputField Input_Password;
+    [SerializeField] Button Btn_Login;
+    [SerializeField] Button Btn_Sign;
+    [SerializeField] Text Text_Error;
+
+
+
+
+
+
+    private void OnEnable()
+    {
+        Btn_Login.onClick.AddListener(OnClick_Login);
+        Btn_Sign.onClick.AddListener(OnClick_Sign);
+    }
+
+    private void OnDisable()
+    {
+        Btn_Login.onClick.RemoveListener(OnClick_Login);
+        Btn_Sign.onClick.RemoveListener(OnClick_Sign);
+    }
+
+    private void Start()
+    {
+        bool DBConnected = DBManager.Instance.LoginUI_OnStart_ConnectDB();
+        if (DBConnected) { Text_Error.text = "DB 연결 성공!"; }
+        else { Text_Error.text = "DB 연결 실패!"; }
+    }
+
+
+
+
+
+
+    public void OnClick_Login()
+    {
+        bool LoginSuccess =  DBManager.Instance.LoginUI_OnClick_Login_SendQuery(Input_Id.text, Input_Password.text);
+        if (LoginSuccess) { Text_Error.text = "로그인 성공!"; }
+        else { Text_Error.text = "로그인 실패!"; }
+    }
+
+    public void OnClick_Sign()
+    {
+        //로직을 통해 요청
+    }
+
+
+}
