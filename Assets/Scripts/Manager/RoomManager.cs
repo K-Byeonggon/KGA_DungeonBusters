d1cb6f8658206777c0ed66a7b7f8caa9e38d6630
K-Byeonggon <govleen79@gmail.com>
2024-06-25 22:15:31 +0900

    [feat] 일단 uid를 Player에 적용하려는 시도를 제외하고 게임이 시작되도록 바꿈
    # Conflicts:
    #	Assets/Scripts/MyNetworkRoomPlayer.cs

--- Assets/Scripts/Manager/RoomManager.cs
+++ Assets/Scripts/Manager/RoomManager.cs
@@ -24,7 +24,17 @@ public class RoomManager
     public void RoomUI_OnClick_Ready_CallCmd()
     {
         Debug.Log("준비");
-        int uid = LoginManager.Instance.UserID;
-        RoomPlayers[uid].GetComponent<NetworkRoomPlayer>().CmdChangeReadyState(true);
+        //int uid = LoginManager.Instance.UserID;
+        //RoomPlayers[uid].GetComponent<NetworkRoomPlayer>().CmdChangeReadyState(true);
+        //지금 딕셔너리에 플레이어가 저장이 되지 않는것 같아요.
+        //플레이어 uid 저장 없이 그냥 룸 입장해서 일단 게임 룰을 완성시켜 봅시다.
+        //그러면 딕셔너리에 저장된 플레이어를 찾아서 준비하는게 아니라 그냥 준비 시켜야 겠네?
+        //어.. 근데 그냥 룸플레이어 어떻게 찾음?
+
+        var localPlayer = NetworkClient.connection.identity.GetComponent<MyNetworkRoomPlayer>();
+        if (localPlayer != null)
+        {
+            localPlayer.CmdChangeReadyState(true);
+        }
     }
 }
--- Assets/Scripts/MyNetworkRoomPlayer.cs
+++ Assets/Scripts/MyNetworkRoomPlayer.cs
@@ -8,13 +8,19 @@ public class MyNetworkRoomPlayer : NetworkRoomPlayer
     [SyncVar][SerializeField] int _uid;
     public int Uid {  get { return _uid; }  set { _uid = value; } }
 
-    public override void OnStartClient()
+    public override void OnClientEnterRoom()
     {
+        //이거도 잠시 uid가져오는 시도는 포기합시다~
+        /*
         int clientUID = LoginManager.Instance.UserID;
         UIDManager.Instance.AddClientUID(connectionToClient, clientUID);
         CmdSendUIDToServer(clientUID);
         _uid = UIDManager.Instance.GetClientUID(connectionToClient);
+        */
     }
+
+
+    //여기서 uid가져오고 있는데?
 
     [Command]
     void CmdSendUIDToServer(int uid)
