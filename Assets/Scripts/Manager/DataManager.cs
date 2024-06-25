3be6cc241f0abce6e48f3b5f427ea7949e2e4d36
K-Byeonggon <govleen79@gmail.com>
2024-06-25 23:45:29 +0900

    [feat] DataManaging - 데이터 로드 Root 경로를 설정할 수 있게 전역변수 선언

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -6,4 +6,5 @@ public class DataManager : MonoBehaviour
 {
     Dictionary<int, Monster> _loadedMonsterList = new Dictionary<int, Monster>();
 
+    private readonly string _dataRootPath = "C:/Unity/DungeonBusters/DataParser";
 }
