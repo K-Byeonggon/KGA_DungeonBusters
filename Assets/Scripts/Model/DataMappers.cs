f98382ff469d98e768bfa4459b535c584c8d28f4
K-Byeonggon <govleen79@gmail.com>
2024-06-25 23:41:20 +0900

    [feat] DataManaging - 모델 추가

---
+++ Assets/Scripts/Model/DataMappers.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster
+{
+    public string Name { get; set; }
+    public int HP { get; set; }
+    public string Reward1 {  get; set; }
+    public string Reward2 { get; set; }
+    public string Reward3 { get; set; }
+}
