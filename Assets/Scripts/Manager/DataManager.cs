5d789fbc65b81c382a007ca8a5852bdab9047179
K-Byeonggon <govleen79@gmail.com>
2024-06-25 23:49:22 +0900

    [feat] DataManaging - 데이터 매니저 싱글턴과 생성시점에서 모든 데이터 읽어오도록

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -4,7 +4,35 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
+    public static DataManager Instance { get; private set; }
+
     Dictionary<int, Monster> _loadedMonsterList = new Dictionary<int, Monster>();
 
     private readonly string _dataRootPath = "C:/Unity/DungeonBusters/DataParser";
+
+    private void Awake()
+    {
+        Instance = this;
+        ReadAllDataOnAwake();
+    }
+
+    private void ReadAllDataOnAwake()
+    {
+        ReadData(nameof(Monster));
+    }
+
+    private void ReadData(string tableName)
+    {
+        switch(tableName)
+        {
+            case "Stage1":
+                ReadMonsterTable(tableName);
+                break;
+        }
+    }
+
+    private void ReadMonsterTable(string tableName)
+    {
+
+    }
 }
