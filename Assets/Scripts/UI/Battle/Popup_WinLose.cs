9cb2b927951a99642526f8bb13a67c9ff5daabb7
K_Byeonggon <govleen79@gmail.com>
2024-07-09 11:07:45 +0900

    [feat] 스테이지 승패를 표시해주는 UI 제작

---
+++ Assets/Scripts/UI/Battle/Popup_WinLose.cs
@@ -0,0 +1,46 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup_WinLose : MonoBehaviour
+{
+    [SerializeField] Button Btn_FrontClose;
+    [SerializeField] Text Text_WinLose;
+    [SerializeField] GameObject Transform_SlotRoot;
+    [SerializeField] GameObject Prefab_CardRed;
+
+    public void UpdateText()
+    {
+        bool stageWin = false;//NewGameManager에 StageWinLose만들고 그거 가져오자.
+
+        if(stageWin) { Text_WinLose.text = "토벌 성공!"; }
+        else { Text_WinLose.text = "토벌 실패.."; }
+    }
+
+    public void RemoveCards()
+    {
+        for (int i = 0; i < Transform_SlotRoot.transform.childCount; i++)
+        {
+            Destroy(Transform_SlotRoot.transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void SetCards()
+    {
+        //이것도 뭐 NewGameManager에 있는거 가져와서 만들자.
+        //플레이어별로 색깔이 다르면 좋겠지만..
+    }
+
+    public void UISetActive(bool active)
+    {
+        this.gameObject.SetActive(active);
+    }
+
+    public void OnClick_FrontClose()
+    {
+        //대충 전면 닫기
+        UISetActive(false);
+    }
+}
